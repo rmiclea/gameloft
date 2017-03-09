46094dedd32bd3ad3dd2e1ad93a88b7a0125fe37
miclea.razvan.d@gmail.com <bogdandobritoiu@Bogdans-MacBook-Pro.local>
2017-03-10 00:16:30 +0200

    gameloft-p1

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+var livereload = require('gulp-livereload');
+var cleanCSS = require('gulp-clean-css');
+var autoprefixer = require('gulp-autoprefixer');
+
+
+
+
+
+gulp.task('sass', function(){
+  return gulp.src('./sass/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename("style.css"))
+    .pipe(livereload())
+    .pipe(autoprefixer())
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest(''));
+});
+
+gulp.task('watch', function () {
+  livereload.listen();
+  gulp.watch('./sass/*.scss', ['sass']);
+});
+
+gulp.task('default', [ 'html', 'css' ]);
---
+++ js/main.js
@@ -0,0 +1,73 @@
+
+// Terms&Conditions Pop Up
+
+$(function () {
+  $('#demo').popup();
+});
+
+// Facebook JavaScript SDK - Log In
+
+
+// Submit and checkbox functionalities
+
+$(function() {
+    $('#checkbox').click(function() {
+        if ($(this).prop('checked') === true) {
+            $('.submit-mask').addClass('inactive');
+            $('.terms-text').removeClass('active');
+        } else {
+            $('.submit-mask').removeClass('inactive');
+        }
+    });
+});
+
+$(function() {
+    $('.submit-mask').on('click', function() {
+        if ($('#checkbox').prop('checked') === false) {
+          $('.terms-text').addClass('active');
+        } else {
+          $('.terms-text').removeClass('active');
+        }
+    });
+});
+
+// Terms popup functionality
+
+$(function() {
+    $('.demo_agree').on("click", function () {
+    $('#demo').popup('hide');
+    $('#checkbox').attr('checked', 'checked');
+    $('.submit-mask').addClass('inactive');
+    $('.terms-text').removeClass('active');
+    });
+});
+
+
+// Product Gallery
+
+
+
+// Product Quantity
+
+$(".ddd").on("click", function () {
+
+var $button = $(this);
+var oldValue = $button.closest('.sp-quantity').find("input.quntity-input").val();
+
+if ($button.text() == "+") {
+    var newVal = parseFloat(oldValue) + 1;
+} else {
+// Don't allow decrementing below zero
+if (oldValue > 1) {
+    var newVal = parseFloat(oldValue) - 1;
+} else {
+    newVal = 1;
+}
+}
+
+$button.closest('.sp-quantity').find("input.quntity-input").val(newVal);
+
+});
+
+
+// Related Products functionalities
